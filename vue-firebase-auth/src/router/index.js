2e518326175bddb1ba990a0e359526cb541362e4
eryeden <ery.eden@gmail.com>
2023-04-16 02:28:34 +0900

    add signup / signin page

--- vue-firebase-auth/src/router/index.js
+++ vue-firebase-auth/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SimpleHelloWorldView from '../views/SimpleHelloWorldView.vue'
+import Signup from "@/components/Signup.vue"
+import Signin from "@/components/Signin.vue"
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,8 +20,18 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+     {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+     {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
     }
-  ]
+   ]
 })
 
 export default router
