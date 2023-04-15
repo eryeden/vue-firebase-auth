6d18633465bf1e34ecf116a045ed5950256c39c9
eryeden <ery.eden@gmail.com>
2023-04-16 03:04:03 +0900

    add signup feature.

--- vue-firebase-auth/src/components/Signup.vue
+++ vue-firebase-auth/src/components/Signup.vue
@@ -1,54 +1,79 @@
 <template>
     <div class="signup">
-      <h2>Sign up</h2>
-      <input type="text" placeholder="Username" v-model="username">
-      <input type="password" placeholder="Password" v-model="password">
-      <button>Register</button>
-      <p>Do you have an account? 
-        <router-link to="/signin">sign in now!!</router-link>
-      </p>
+        <h2>Sign up</h2>
+        <input type="text" placeholder="Username" v-model="username">
+        <input type="password" placeholder="Password" v-model="password">
+        <button @click="signup">Register</button>
+        <p>Do you have an account?
+            <router-link to="/signin">sign in now!!</router-link>
+        </p>
     </div>
 </template>
   
-  <script>
-  export default {
+<script>
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+export default {
     name: 'Signup',
-    data () {
-      return {
-        username: '',
-        password: ''
-      }
+    data() {
+        return {
+            username: '',
+            password: '',
+        }
     },
-    methods: {}
-  }
-  </script>
+    methods: {
+        signup() {
+            const auth = getAuth();
+            createUserWithEmailAndPassword(auth, this.username, this.password)
+                .then((userCredential) => {
+                    // Signed in
+                    const user = userCredential.user;
+                    alert('Create account: ', userCredential.email)
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    alert(error.message)
+                });
+
+        }
+    }
+}
+</script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  h1, h2 {
+<style scoped>
+h1,
+h2 {
     font-weight: normal;
-  }
-  ul {
+}
+
+ul {
     list-style-type: none;
     padding: 0;
-  }
-  li {
+}
+
+li {
     display: inline-block;
     margin: 0 10px;
-  }
-  a {
+}
+
+a {
     color: #42b983;
-  }
-  .signup {
+}
+
+.signup {
     margin-top: 20px;
-  
+
     display: flex;
     flex-flow: column nowrap;
     justify-content: center;
     align-items: center
-  }
-  input {
+}
+
+input {
     margin: 10px 0;
     padding: 10px;
-  }
-  </style>
+}
+</style>
--- vue-firebase-auth/src/main.js
+++ vue-firebase-auth/src/main.js
@@ -2,6 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Firebase config
+import { initializeApp } from "firebase/app";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyADprDnvky9DAVczpD3bHhsWlCN3ORRmyc",
+    authDomain: "vue-firebase-auth-4c16c.firebaseapp.com",
+    projectId: "vue-firebase-auth-4c16c",
+    storageBucket: "vue-firebase-auth-4c16c.appspot.com",
+    messagingSenderId: "853652868582",
+    appId: "1:853652868582:web:e46b56adfbb0d30f1cda27",
+    measurementId: "G-ZWQQLFW718"
+  };
+  
+// Initialize Firebase
+const firebase_app = initializeApp(firebaseConfig);
+
 import './assets/main.css'
 
 const app = createApp(App)
