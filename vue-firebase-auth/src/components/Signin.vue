240554f3653c00af7dbd59ab32565b97618b8742
eryeden <ery.eden@gmail.com>
2023-04-16 03:08:59 +0900

    add a signin feature.

--- vue-firebase-auth/src/components/Signin.vue
+++ vue-firebase-auth/src/components/Signin.vue
@@ -1,54 +1,80 @@
 <template>
     <div class="signin">
-      <h2>Sign in</h2>
-      <input type="text" placeholder="Username" v-model="username">
-      <input type="password" placeholder="Password" v-model="password">
-      <button>Signin</button>
-      <p>You don't have an account? 
-        <router-link to="/signup">create account now!!</router-link>
-      </p>
+        <h2>Sign in</h2>
+        <input type="text" placeholder="Username" v-model="username">
+        <input type="password" placeholder="Password" v-model="password">
+        <button @click="signin">Signin</button>
+        <p>You don't have an account?
+            <router-link to="/signup">create account now!!</router-link>
+        </p>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+export default {
     name: 'Signin',
-    data () {
-      return {
-        username: '',
-        password: ''
-      }
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
     },
-    methods: {}
-  }
-  </script>
+    methods: {
+        signin() {
+            const auth = getAuth();
+            signInWithEmailAndPassword(auth, this.username, this.password)
+                .then((userCredential) => {
+                    // Signed in
+                    const user = userCredential.user;
+                    // ...
+                    alert("You are succeeded to log in.");
+                    this.$router.push("/")
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    alert(error.message)
+                });
+
+        }
+    }
+}
+</script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  h1, h2 {
+<style scoped>
+h1,
+h2 {
     font-weight: normal;
-  }
-  ul {
+}
+
+ul {
     list-style-type: none;
     padding: 0;
-  }
-  li {
+}
+
+li {
     display: inline-block;
     margin: 0 10px;
-  }
-  a {
+}
+
+a {
     color: #42b983;
-  }
-  .signin {
+}
+
+.signin {
     margin-top: 20px;
-  
+
     display: flex;
     flex-flow: column nowrap;
     justify-content: center;
     align-items: center
-  }
-  input {
+}
+
+input {
     margin: 10px 0;
     padding: 10px;
-  }
-  </style>
+}
+</style>
